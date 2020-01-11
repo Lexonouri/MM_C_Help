dc2bafec7dbd8bb4528b1becc7e1fdefa232e8de
Mateusz Janota <mateusz@dnacloud.pl>
2020-01-11 12:44:50 +0100

    Exercise 3

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ int zad1() {
     //Komunikat dla usera
     printf("Wpisz liczby do obliczenia pierwiastków:\n");
 
-    //Iteracja pobierająca dane od tablicy i wyliczajaca pierwiastki do drugiego wiersza
+    //Iteracja pobierająca dane od usera i wyliczajaca pierwiastki do drugiego wiersza
     for(int i = 0; i < 5; i++) {
         printf("Liczba nr %d: \n", i+1);
         scanf("%d", &A[0][i]);
@@ -36,7 +36,7 @@ float zad2() {
     //Komunikat dla usera
     printf("Wpisz liczby do tablicy:\n");
 
-    //Iteracja pobierająca dane od tablicy i wyliczajaca pierwiastki do drugiego wiersza
+    //Iteracja pobierająca dane od usera i wyliczajaca pierwiastki do drugiego wiersza
     for(int i = 0; i < 5; i++) {
         printf("Liczba nr %d: \n", i+1);
         scanf("%f", &T[0][i]);
@@ -48,7 +48,7 @@ float zad2() {
     max = T[0][0];
 
     //Iteracja sprawdzająca wartości
-    for(int j=1; j<5; j++)
+    for(int j=0; j<5; j++)
     {
         //Jeżeli aktualny element jest większy
         if(T[0][j] > max)
@@ -68,13 +68,86 @@ float zad2() {
     printf("Najniższa wartość: %f\n", min);
 }
 
+//Zadanie 3
+int zad3() {
+
+    //Inicjowanie macierzy 4x3
+    int M[3][4], min, max, sum = 0, chosen_number = 0;
+
+    //Komunikat dla usera
+    printf("Wpisz liczby do macierzy:\n");
+
+    //Iteracja pobierająca dane do macierzy
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 4; j++) {
+            printf("Liczba o indeksie %d x %d\n", i, j);
+            scanf("%d", &M[i][j]);
+            printf("\n");
+        }
+    }
+
+    //Wyświetlenie danych
+    printf("Wizualizacja macierzy:\n");
+    printf("[ %d  %d  %d  %d ]\n", M[0][0], M[0][1], M[0][2], M[0][3]);
+    printf("[ %d  %d  %d  %d ]\n", M[1][0], M[1][1], M[1][2], M[1][3]);
+    printf("[ %d  %d  %d  %d ]\n\n", M[2][0], M[2][1], M[2][2], M[2][3]);
+
+    //Pobieranie numeru wiersza do obliczeń
+    printf("Wpisz numer wiersza (0, 1, 2) na którym chcesz wykonać obliczenia\n");
+    scanf("%d", &chosen_number);
+
+    //Test na dzbana
+    if(chosen_number >= 0 && chosen_number < 3) {
+
+        //Komunikat
+        printf("Wybrałeś wiersz numer: %d\n\n", chosen_number);
+
+        //Przypisywanie wartosci poczatkowych dla min i max
+        min = M[chosen_number][0];
+        max = M[chosen_number][0];
+
+        //Iteracja sprawdzająca wartości
+        for(int l=0; l<4; l++) {
+
+            //sumowanie wybranego wiersza
+            sum += M[chosen_number][l];
+
+            //Jeżeli aktualny element jest większy
+            if(M[chosen_number][l] > max)
+            {
+                max = M[chosen_number][l];
+            }
+
+            //Jeżeli aktualny element jest mniejszy
+            if(M[chosen_number][l] < min)
+            {
+                min = M[chosen_number][l];
+            }
+        }
+
+        //Podawanie wyników
+        printf("Najwyższa wartość dla wiersza nr %d: %d\n", chosen_number, max);
+        printf("Najniższa wartość dla wiersza nr %d: %d\n\n", chosen_number, min);
+        printf("Suma dla wiersza nr %d: %d\n\n", chosen_number, sum);
+
+    } else {
+
+        printf("Wpisano złą liczbe, wiesz o tym...");
+
+    }
+
+}
+
 int main() {
 
     //Wywołanie zadania nr 1
     //zad1();
 
     //Wywołanie zadania nr 2
-    zad2();
+    //zad2();
+
+    //Wywołanie zadania nr 3
+    zad3();
 
     return 0;
 
